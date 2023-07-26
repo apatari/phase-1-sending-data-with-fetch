5f80bc4f7e7d268906e8cebc682c1e11274db19f
Andy Patari <andy.patari@gmail.com>
2023-07-26 13:06:08 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    const user = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => addToBody(data.id))
+    .catch(error => {
+        document.querySelector('body').append(error.message)
+    })
+}
+
+const addToBody = (id) => {
+    document.querySelector('body').append(id)
+}
